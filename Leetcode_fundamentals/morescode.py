eb45dbbd8adac975defeca25da24c0073ebba68e
Dikshanta <3108dikshanta@gmail.com>
2024-07-08 12:00:00 +0545

    commit: leetcode

---
+++ Leetcode_fundamentals/morescode.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",
+                      ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+                      "...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        # Set to store unique Morse code representations
+        transformations = set()
+
+        for word in words:
+            morse_word = ''.join(morse_code[ord(char) - ord('a')] for char in word)
+            transformations.add(morse_word)
+
+        return len(transformations)

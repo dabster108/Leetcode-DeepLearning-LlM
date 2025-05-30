4f50b5f9bba467289c5eecd5bedfb177dae430b2
Dikshanta <3108dikshanta@gmail.com>
2025-05-30 10:12:31 +0545

    logisitc regression added

--- SumarSirAi/logistic regression-77db65e9-32e4-4971-b308-34b63a64ed9b.py
+++ SumarSirAi/logistic regression-77db65e9-32e4-4971-b308-34b63a64ed9b.py
@@ -44,4 +44,5 @@ y_pred = model.predict(X_test_scaled)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print(model.classes_)
 

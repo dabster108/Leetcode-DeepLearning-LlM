d056da198e7d0668c0a8ebe4ca1b088d4aef2349
Dikshanta <3108dikshanta@gmail.com>
2025-05-31 22:32:11 +0545

    leetcode added

--- SumarSirAi/logistic regression-77db65e9-32e4-4971-b308-34b63a64ed9b.py
+++ SumarSirAi/logistic regression-77db65e9-32e4-4971-b308-34b63a64ed9b.py
@@ -44,5 +44,4 @@ y_pred = model.predict(X_test_scaled)
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("\nClassification Report:\n", classification_report(y_test, y_pred))
-print(model.classes_)
-
+print(model.classes_)  

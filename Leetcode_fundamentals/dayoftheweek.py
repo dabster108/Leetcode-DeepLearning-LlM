042d7b028ac0be9c67d258f902fc53072408027a
Dikshanta <3108dikshanta@gmail.com>
2025-06-21 21:56:19 +0545

    leetcode added

---
+++ Leetcode_fundamentals/dayoftheweek.py
@@ -0,0 +1,6 @@
+import datetime
+
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        date = datetime.date(year, month, day)
+        return date.strftime("%A")
